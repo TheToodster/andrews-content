bc8adaae3a8d8dd1048a03d40f989981c8f28336
TheToodster <48666656+TheToodster@users.noreply.github.com>
2024-08-14 20:31:38 +0100

    Delete toods-dispenser-tool directory
    Outdated

--- toods-dispenser-tool/lua/entities/toods_dispenser/shared.lua
+++
@@ -1,9 +0,0 @@
-ENT.Type             = "anim"
-ENT.Base             = "base_anim"
-ENT.PrintName    = "Dispenser 1"
-ENT.Category        = "Veeti's and Tood's Drop Pods"
-
-ENT.Spawnable = true
-ENT.AdminSpawnable = true
-ENT.AdminOnly = false
-ENT.DoNotDuplicate = true
